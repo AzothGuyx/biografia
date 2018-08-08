bed175ea4f1d39c6947fd8c0b80c0309068db31b
AzothGuyx <cristianxapata@gmail.com>
2018-08-07 20:12:56 -0500

    avance de pagina

---
+++ Functions.js
@@ -0,0 +1,13 @@
+function nuevo(){
+    var name =document.getElementById('txtNombre')
+    var pass=document.getElementById('txtPass')
+    console.log(name)
+    console.log(pass)
+    alert('hola mundo')
+
+}
+
+function test(){
+    
+    alert('la espada invisible es la mas mortifera')
+}
